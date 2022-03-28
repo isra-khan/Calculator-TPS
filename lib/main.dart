b9b492c9e6485246579ff5d8861b5c2842d93c09
Isra Khan <Isra.Khan@tpsonline.com>
2022-03-28 11:46:19 +0500

    changes added

--- lib/main.dart
+++ lib/main.dart
@@ -133,6 +133,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData.light(),
+      darkTheme: ThemeData.dark(),
 
       debugShowCheckedModeBanner: false,
       home:SafeArea(child:Scaffold(
